231169da4ec73e728e7f8bf373a23e8b02711144
lenov <lenov@Idepad_Gaming_3>
2025-07-24 09:53:43 +0530

    Day3 Assignment

---
+++ C2TC_ASSIGMENT/src/Assignment2/StudentDemo.java
@@ -0,0 +1,14 @@
+package Assignment2;
+
+public class StudentDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Student s1 = new Student("Navin", 20, "IT");
+
+	        // Calling method
+	        s1.displayDetails();
+
+	
+
+}
+}
